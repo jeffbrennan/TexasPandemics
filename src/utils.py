a3744d7f0b5cfb90a8a22532f481f4602d629277
jeffbrennan <jeffbrennan10@gmail.com>
2023-05-25 19:34:29 -0400

    add date to file name

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,11 @@
 import pandas as pd
+from datetime import datetime as dt
 
 
 def write_file(df: pd.DataFrame, table_path: str) -> None:
     print(f'Writing file to {table_path}')
-    df.to_csv(f'{table_path}.csv', index=False)
+    current_date = dt.now().strftime('%Y_%m_%d')
+    df.to_csv(f'{table_path}_{current_date}.csv', index=False)
 
 
 def load_csv(url: str) -> pd.DataFrame:
