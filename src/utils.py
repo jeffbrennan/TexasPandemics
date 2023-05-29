0b0c91131a1753bd85540d83e58233fb9716a2af
jeffbrennan <jeffbrennan10@gmail.com>
2023-05-29 18:05:30 -0400

    add parquet read and date option to write

--- src/utils.py
+++ src/utils.py
@@ -2,15 +2,22 @@ import pandas as pd
 from datetime import datetime as dt
 
 
-def write_file(df: pd.DataFrame, table_path: str) -> None:
+def write_file(df: pd.DataFrame, table_path: str, add_date: bool = True) -> None:
     print(f'Writing file to {table_path}')
-    current_date = dt.now().strftime('%Y_%m_%d')
-    df.to_csv(f'{table_path}_{current_date}.csv', index=False)
+    if add_date:
+        current_date = dt.now().strftime('%Y_%m_%d')
+        df.to_csv(f'{table_path}_{current_date}.csv', index=False)
+    else:
+        df.to_csv(f'{table_path}.csv', index=False)
 
 
 def load_csv(url: str) -> pd.DataFrame:
     df = pd.read_csv(url)
     return df
+
+
+def load_parquet(url: str) -> pd.DataFrame:
+    return pd.read_parquet(url)
 
 
 def convert_date(date_series: pd.Series, date_format: str) -> pd.Series:
