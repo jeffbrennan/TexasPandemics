861d1493643923cf0779cbde3e998059b2c9fc86
jeffbrennan <jeffbrennan10@gmail.com>
2023-06-01 18:36:11 -0400

    add function to invoke r script from python

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from datetime import datetime as dt
+import subprocess
 
 
 def write_file(df: pd.DataFrame, table_path: str, add_date: bool = True) -> None:
@@ -25,3 +26,34 @@ def convert_date(date_series: pd.Series, date_format: str) -> pd.Series:
         return pd.to_datetime(date_series * 1_000_000).dt.date
 
     return pd.to_datetime(date_series, format=date_format).dt.date
+
+
+# TODO: generalize config args
+def run_r_script(config: dict) -> bool:
+    # https://stackoverflow.com/questions/62716500/call-and-execute-an-r-script-from-python
+    try:
+        p = subprocess.Popen(
+            ["Rscript",
+             config['arg'],
+             config['script_path'],
+             config['input_file'],
+             config['output_file']
+             ],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+
+        output, error = p.communicate()
+
+        if p.returncode == 0:
+            print('R OUTPUT:\n {0}'.format(output.decode("utf-8")))
+        else:
+            print('R ERROR:\n {0}'.format(error.decode("utf-8")))
+
+        return True
+
+    except Exception as e:
+        print(e)
+
+        return False
