7bff708bb61013bd83db0e452e64f8193b04f213
jeffbrennan <jeffbrennan10@gmail.com>
2023-08-25 09:15:40 +0900

    handle new file format

--- etl/assets/origin/wastewater/get_biobot_wastewater.py
+++ etl/assets/origin/wastewater/get_biobot_wastewater.py
@@ -20,6 +20,6 @@ from src.wastewater.get_biobot_wastewater import (
 )
 def get_biobot_wastewater() -> pd.DataFrame:
     wastewater_run_data = obtain_urls(1)
-    raw_data = [pd.read_csv(i) for i in wastewater_run_data['urls']]
+    raw_data = [pd.read_csv(filepath_or_buffer=i, skiprows=1) for i in wastewater_run_data['urls']]
     cleaned_biobot_data = pd.concat([clean_data(i) for i in raw_data]).drop_duplicates()
     return cleaned_biobot_data
