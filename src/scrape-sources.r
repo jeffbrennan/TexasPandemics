06de0bb0620abc778e0418ae907f50be6d59d914
jeffbrennan <jeffbrennan10@gmail.com>
2023-09-09 11:04:58 -0400

    remove pre post-emergency code & fies

--- src/scrape-sources.r
+++
@@ -1,9 +0,0 @@
-library(rmarkdown)
-
-base_wd = "C:/Users/jeffb/Desktop/Life/personal-projects/COVID"
-setwd(base_wd)
-
-Sys.setenv(RSTUDIO_PANDOC = 'C:/Program Files/RStudio/bin/quarto/bin/tools')
-render('src/covid-scraping.rmd', output_dir = 'diagnostics/', knit_root_dir = base_wd)
-render('src/statistical-analysis.rmd', output_dir = 'diagnostics/', knit_root_dir = base_wd)
-render('src/diagnostics.rmd', output_dir = 'diagnostics/', knit_root_dir = base_wd)
